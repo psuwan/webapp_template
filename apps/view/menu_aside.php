12fa7ae0401af82399b7a8e2ba8ddb7a433c223b
Pattanapong Suwan <psuwan@yahoo.com>
2023-10-15 08:02:50 +0700

    2023-10-15 separated footer

--- apps/view/menu_aside.php
+++ apps/view/menu_aside.php
@@ -3,7 +3,7 @@
         <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
         <a class="navbar-brand m-0" href=" https://demos.creative-tim.com/soft-ui-dashboard-pro/pages/dashboards/default.html " target="_blank">
             <img src="./assets_softui/img/cylab_logo.png" class="navbar-brand-img h-100" alt="main_logo">
-            <span class="ms-1 font-weight-bold"> CY's Lab ... </span>
+            <span class="ms-1 font-weight-bold"> VETCY's Lab ... </span>
         </a>
     </div>
     <hr class="horizontal dark mt-0">
