0172e41d005625330da4037c8a2539cb5f698432
Pattanapong Suwan <psuwan@yahoo.com>
2023-10-15 12:41:05 +0700

    2023-10-15 completed smrtiot layout

--- apps/view/menu_aside.php
+++ apps/view/menu_aside.php
@@ -2,8 +2,8 @@
     <div class="sidenav-header">
         <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
         <a class="navbar-brand m-0" href=" https://demos.creative-tim.com/soft-ui-dashboard-pro/pages/dashboards/default.html " target="_blank">
-            <img src="./assets_softui/img/cylab_logo.png" class="navbar-brand-img h-100" alt="main_logo">
-            <span class="ms-1 font-weight-bold"> VETCY's Lab ... </span>
+            <img src="./assets_softui/img/apks_smrtiot.png" class="navbar-brand-img h-100" alt="main_logo">
+            <span class="ms-1 font-weight-bold"> Smart IoT... </span>
         </a>
     </div>
     <hr class="horizontal dark mt-0">
