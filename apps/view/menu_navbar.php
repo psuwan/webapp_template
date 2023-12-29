b94b4d2b8c5b561e1bb2c68ba1fe262677b30187
Pattanapong Suwan <psuwan@yahoo.com>
2023-12-29 15:30:19 +0700

    20231229-back to work here

--- apps/view/menu_navbar.php
+++ apps/view/menu_navbar.php
@@ -61,6 +61,7 @@
                 </div>
             </a>
         </div>
+
         <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
             <div class="ms-md-auto pe-md-3 d-flex align-items-center">
                 <div class="input-group">
@@ -69,13 +70,8 @@
                 </div>
             </div>
             <ul class="navbar-nav  justify-content-end">
-                <li class="nav-item d-flex align-items-center">
-                    <a href="../../pages/authentication/signin/illustration.html" class="nav-link text-body font-weight-bold px-0" target="_blank">
-                        <i class="fa fa-user me-sm-1"></i>
-                        <span class="d-sm-inline d-none">Sign In</span>
-                    </a>
-                </li>
-                <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
+
+                <li class="nav-item d-xl-none d-flex align-items-center">
                     <a href="javascript:;" class="nav-link text-body p-0" id="iconNavbarSidenav">
                         <div class="sidenav-toggler-inner">
                             <i class="sidenav-toggler-line"></i>
@@ -84,6 +80,15 @@
                         </div>
                     </a>
                 </li>
+
+                <li class="nav-item d-flex align-items-center ps-3">
+                    <a href="../../pages/authentication/signin/illustration.html" class="nav-link text-body font-weight-bold px-0" target="_blank">
+                        <i class="fa fa-user me-sm-1"></i>
+                        <span class="d-sm-inline d-none">Sign In</span>
+                    </a>
+                </li>
+
+
                 <li class="nav-item px-3 d-flex align-items-center">
                     <a href="javascript:;" class="nav-link text-body p-0">
                         <i class="fa fa-cog fixed-plugin-button-nav cursor-pointer"></i>
